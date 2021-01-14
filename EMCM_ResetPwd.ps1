ccf1442640dbce85576c7935be87f7032f980546
dseferiadis <dave@seferiadis.com>
2021-01-13 22:24:44 -0500

    Updated PowerShell Automation Scripts

---
+++ EMCM_ResetPwd.ps1
@@ -0,0 +1,187 @@
+﻿# PowerShell Script to Import CSV of Student and Teachers 
+# and reset their passwords and export results
+
+$inputFile = $PSScriptRoot + "\" + "EMCM_Reset.csv"
+$outputFile = $PSScriptRoot + "\" + "EMCM_ResetResults.csv"
+
+# Flag to Manually Validate Change Actions - either 0 (No) or 1 (Yes)
+$confirmchanges = 1
+
+# Function to Validate CSV File Columns
+function Import-ValidCSV
+{
+        (
+                [parameter(Mandatory=$true)]
+                [ValidateScript({test-path $_ -type leaf})]
+                [string]$inputFile,
+                [string[]]$requiredColumns
+        )
+        $csvImport = import-csv $inputFile
+        $inputTest = $csvImport | gm
+        foreach ($requiredColumn in $requiredColumns)
+        {
+                if (!($inputTest | ? {$_.name -eq $requiredColumn}))
+                {
+                        write-error "$inputFile is missing the $requiredColumn column"
+                        exit 10
+                }
+        }
+        $csvImport
+}
+
+function NameToUserPrincipal($fn, $ln, $rl)
+{
+    #Strip all Non Word Characters and make lowercase from FirstName and LastName so we can create a consistent email format
+    $EmailFirstname = $fn
+    $EmailFirstname = $EmailFirstname -replace '[\W]', ''
+    $EmailFirstname = $EmailFirstname.ToLower()
+
+    $EmailLastname = $ln
+    $EmailLastname = $EmailLastname -replace '[\W]', ''
+    $EmailLastname = $EmailLastname.ToLower()
+
+    #Get User Role, Remove Non Word Characters and Make Lowercase
+    $UserRole = $rl
+    $UserRole = $UserRole -replace '[\W]', ''
+    $UserRole = $UserRole.ToLower()
+
+    #Format Email Address / UserPrincipalName
+    $UserPrincipalName = $EmailFirstname + "." + $EmailLastname + "@" + $UserRole + "." + "ellismarsaliscenter.org"
+    $UserPrincipalName = $UserPrincipalName.ToLower()
+
+    return $UserPrincipalName
+}
+
+function CheckForDuplicates($inputFile)
+{
+    $DuplicateQty = 0
+    $RowNum = 0
+    $Users = Import-Csv -Path $inputFile
+    $RefUsers = Import-Csv -Path $inputFile
+    foreach($User in $Users){
+        $RowNum = $RowNum + 1
+        $UserPrincipalName = NameToUserPrincipal $User.FirstName $User.LastName $User.Role
+        $UserQty = 0
+        foreach($RefUser in $RefUsers){
+            $RefUserPrincipalName = NameToUserPrincipal $RefUser.FirstName $RefUser.LastName $RefUser.Role
+            if($RefUserPrincipalName -eq $UserPrincipalName){
+                $UserQty = $UserQty + 1
+            }
+        }
+        if($UserQty -gt 1){
+            Write-Host "Row" $RowNum $UserPrincipalName "is duplicated in import list! Resolve conflict before proceeding"
+            $DuplicateQty = $DuplicateQty + 1
+        }
+    }
+    return $DuplicateQty
+}
+
+function CheckPhoneSyntax($inputFile)
+{
+    $InvalidQty = 0
+    $RowNum = 0
+    $Users = Import-Csv -Path $inputFile
+    foreach($User in $Users){
+        $UserInvalidPhone = 0
+        $RowNum = $RowNum + 1
+        $UserPrincipalName = NameToUserPrincipal $User.FirstName $User.LastName $User.Role
+        if($User.MoblePhone -match "^\d+$"){
+            # Number is numeric
+            if($User.MoblePhone.Length -eq 10){
+                # Number is numeric and 10 digits - this is valid
+            } else {
+                # Number is numeric but not 10 digits - this is invalid
+                $UserInvalidPhone = $UserInvalidPhone + 1
+                $InvalidQty = $InvalidQty + 1
+            }
+        } else {
+            Write-Host "NotNumeric"
+            $UserInvalidPhone = $UserInvalidPhone + 1
+            $InvalidQty = $InvalidQty + 1
+        }
+
+        if($UserInvalidPhone -gt 0){
+            Write-Host "Row" $RowNum $UserPrincipalName "has invalid phone number and/or format!" $User.MoblePhone "Resolve before proceeding"
+            $DuplicateQty = $DuplicateQty + 1
+        }
+    }
+    return $InvalidQty
+}
+
+# Define Required Columns
+[string[]]$requiredColumns = "FirstName","LastName","PreferredFirstName","PersonalEmail","MoblePhone","Role"
+Write-Host "Importing CSV of Target User List"
+$error.clear()
+try { 
+    $testCsv = Import-ValidCsv $inputFile $requiredColumns
+}
+catch { 
+    "Failed to Import CSV - Malformed File"
+    Write-Host $error
+}
+if (!$error) { 
+    "CSV Syntax is Valid" 
+}
+
+# Check for Duplicate Users in Import CSV
+Write-Host "Checking for Duplicates"
+$DuplicateCheck = CheckForDuplicates $inputFile
+if($DuplicateCheck -gt 0){
+    Write-Host "Duplicate User(s) Detected"
+    exit
+} else {
+    Write-Host "No Duplicate User(s) Detected"
+}
+
+# Check Phone Number is in Valid Format
+Write-Host "Checking Phone Number Formatting"
+$PhoneCheck = CheckPhoneSyntax $inputFile
+if($PhoneCheck -gt 0){
+    Write-Host "Phone Number Validations Failed"
+    exit
+} else {
+    Write-Host "Phone Number Validations Passed"
+}
+
+# If Validation of CSV Passed - Proceed to Load into Target Variable
+$Users = Import-Csv -Path $inputFile
+
+#Connect to Azure AD
+Write-Host "Connecting to MSOnline and AzureAD"
+Connect-MsolService
+Connect-AzureAD
+
+#Get all Azure AD Users 
+$MsolUsers = Get-MsolUser -All
+
+# Reset Output File and Write Header
+Out-File -FilePath $outputFile
+Add-Content -Path $outputFile -Value "UserPrincipalName,Password,ResetDate"
+
+foreach($User in $Users){
+    $UserPrincipalName = NameToUserPrincipal $User.FirstName $User.LastName $User.Role
+    
+    #Check to See if Target Account Already Exists
+    $UserExists = 0
+    
+    foreach($Msoluser in $MsolUsers){
+    if ($Msoluser.UserPrincipalName -eq $UserPrincipalName){
+        $UserExists++
+        break
+        }
+    }
+
+    #Reset Password if Target User is Found
+    if ($UserExists -eq 0) {
+        Write-Host "$UserPrincipalName not found in Azure AD"
+        Add-Content -Path $outputFile -Value "$UserPrincipalName,,$currentDate"
+    } else {
+        Write-Host "Resetting Password for: " $UserPrincipalName
+        if($confirmchanges -eq 1){ Read-Host -Prompt "Press any key to continue or CTRL-C to quit" }
+        $userpwd = Set-MsolUserPassword -UserPrincipalName $UserPrincipalName
+        $currentDate = Get-Date
+        Add-Content -Path $outputFile -Value "$UserPrincipalName,$userpwd,$currentDate"
+    }
+}
+
+Write-Host "Output Complete"
--- EMCM_UserSync.ps1
+++ EMCM_UserSync.ps1
@@ -1,7 +1,14 @@
-# PowerShell Script to Import CSV of Student and Teachers 
-# from SalesForce and create accounts that do not already exist
-# and update accounts that already exist based on the email address as a
-# primary key that does not change
+# PowerShell Script to Sync a CSV list of Student and Teachers 
+# and create users that don't exist in both Azure AD and Canvas
+# or update users whose attributes have changed, and delete users 
+# no longer in the import list with a soft delete wait period where
+# accounts will be disabled for X days until actual deletion
+
+# Source CSV File
+$inputFile = $PSScriptRoot + "\" + "EMCM_Import.csv"
+$SettingsFile =  $PSScriptRoot + "\" + "CanvasApiKey.txt"
+$Office365StudentTeacherUsers = $PSScriptRoot + "\" + "Office365StudentTeacherUsers.csv"
+$Office365AllUsers = $PSScriptRoot + "\" + "Office365AllUsers.csv"
 
 # Flag to Manually Validate Change Actions - either 0 (No) or 1 (Yes)
 $confirmchanges = 1
@@ -9,8 +16,75 @@ $confirmchanges = 1
 # Define days to wait to process deletion of account after account is not in import CSV
 $deletewaitdays = 30
 
+# Load Canvas API Key
+Get-Content $SettingsFile | foreach-object -begin {$h=@{}} -process { $k = [regex]::split($_,'='); if(($k[0].CompareTo("") -ne 0) -and ($k[0].StartsWith("[") -ne $True)) { $h.Add($k[0], $k[1]) } }
+$canvasToken = $h.Get_Item("canvastoken")
+
+# Check if Account Anchor Exists in Canvas
+function ExistsInCanvas($CanvasUsername){
+    $canvasDomain = "https://emcm.instructure.com/api/v1"
+    $accessToken = $canvasToken
+    $headers = @{"Authorization"="Bearer "+$accessToken}  # build access token header
+    $canvasUserUrl = "$canvasDomain/accounts/self/users?search_term=$CanvasUsername"
+    
+    Write-Host "Attempting API Call to:" $canvasUserUrl
+    try{
+        $response = Invoke-RestMethod -Method GET -uri $canvasUserUrl -header $headers
+        if($response.login_id -eq $CanvasUsername){
+            return $true
+        } else {
+            return $false
+        }
+    } catch {
+        return $false
+    }
+}
+
+# Create Canvas Anchor Account
+function CreateInCanvas($CanvasUsername){
+    $canvasDomain = "https://emcm.instructure.com/api/v1"
+    $accessToken = $canvasToken
+    $headers = @{"Authorization"="Bearer "+$accessToken}  # build access token header
+    $canvasUserUrl = "$canvasDomain/accounts/self/users?search_term=$CanvasUsername"
+    
+    Write-Host "Attempting API Call to:" $canvasUserUrl
+    try{
+        $response = Invoke-RestMethod -Method GET -uri $canvasUserUrl -header $headers
+        if($response.login_id -eq $CanvasUsername){
+            return $true
+        } else {
+            return $false
+        }
+    } catch {
+        return $false
+    }
+}
+
+function DeleteInCanvas($CanvasUsername){
+    $canvasDomain = "https://emcm.instructure.com/api/v1"
+    $accessToken = "19089~oe2Z8IoLu5Oi8FjWaikdqiYSI9r1hfpEG58XSBvhhu5Hv9v2eMWG5aQLezbTcvFX"
+    $headers = @{"Authorization"="Bearer "+$accessToken}  # build access token header
+    $canvasUserUrl = "$canvasDomain/accounts/self/users?search_term=$CanvasUsername"
+    
+    Write-Host "Attempting API Call to:" $canvasUserUrl
+    try{
+        $response = Invoke-RestMethod -Method GET -uri $canvasUserUrl -header $headers
+        if($response.login_id -eq $CanvasUsername){
+            # User Exists - Get id to process next delete call
+            $canvasuserid = $response.id
+            $canvasUserUrl = "$canvasDomain/accounts/self/users/$canvasuserid"
+            Write-Host $canvasUserUrl
+            $response = Invoke-RestMethod -Method DELETE -uri $canvasUserUrl -header $headers
+            return $true
+        } else {
+            return $false
+        }
+    } catch {
+        return $false
+    }
+}
+
 # Function to Validate CSV File Columns
-$inputFile = $PSScriptRoot + "\\" + "EMCM_Import.csv"
 function Import-ValidCSV
 {
         (
@@ -55,6 +129,62 @@ function NameToUserPrincipal($fn, $ln, $rl)
     return $UserPrincipalName
 }
 
+function CheckForDuplicates($inputFile)
+{
+    $DuplicateQty = 0
+    $RowNum = 0
+    $Users = Import-Csv -Path $inputFile
+    $RefUsers = Import-Csv -Path $inputFile
+    foreach($User in $Users){
+        $RowNum = $RowNum + 1
+        $UserPrincipalName = NameToUserPrincipal $User.FirstName $User.LastName $User.Role
+        $UserQty = 0
+        foreach($RefUser in $RefUsers){
+            $RefUserPrincipalName = NameToUserPrincipal $RefUser.FirstName $RefUser.LastName $RefUser.Role
+            if($RefUserPrincipalName -eq $UserPrincipalName){
+                $UserQty = $UserQty + 1
+            }
+        }
+        if($UserQty -gt 1){
+            Write-Host "Row" $RowNum $UserPrincipalName "is duplicated in import list! Resolve conflict before proceeding"
+            $DuplicateQty = $DuplicateQty + 1
+        }
+    }
+    return $DuplicateQty
+}
+
+function CheckPhoneSyntax($inputFile)
+{
+    $InvalidQty = 0
+    $RowNum = 0
+    $Users = Import-Csv -Path $inputFile
+    foreach($User in $Users){
+        $UserInvalidPhone = 0
+        $RowNum = $RowNum + 1
+        $UserPrincipalName = NameToUserPrincipal $User.FirstName $User.LastName $User.Role
+        if($User.MoblePhone -match "^\d+$"){
+            # Number is numeric
+            if($User.MoblePhone.Length -eq 10){
+                # Number is numeric and 10 digits - this is valid
+            } else {
+                # Number is numeric but not 10 digits - this is invalid
+                $UserInvalidPhone = $UserInvalidPhone + 1
+                $InvalidQty = $InvalidQty + 1
+            }
+        } else {
+            Write-Host "NotNumeric"
+            $UserInvalidPhone = $UserInvalidPhone + 1
+            $InvalidQty = $InvalidQty + 1
+        }
+
+        if($UserInvalidPhone -gt 0){
+            Write-Host "Row" $RowNum $UserPrincipalName "has invalid phone number and/or format!" $User.MoblePhone "Resolve before proceeding"
+            $DuplicateQty = $DuplicateQty + 1
+        }
+    }
+    return $InvalidQty
+}
+
 # Define Required Columns
 [string[]]$requiredColumns = "FirstName","LastName","PreferredFirstName","PersonalEmail","MoblePhone","Role"
 Write-Host "Importing CSV of Target User List"
@@ -67,8 +197,29 @@ catch {
     Write-Host $error
 }
 if (!$error) { 
-    "Valid CSV File Found!" 
+    "CSV Syntax is Valid" 
 }
+
+# Check for Duplicate Users in Import CSV
+Write-Host "Checking for Duplicates"
+$DuplicateCheck = CheckForDuplicates $inputFile
+if($DuplicateCheck -gt 0){
+    Write-Host "Duplicate User(s) Detected"
+    exit
+} else {
+    Write-Host "No Duplicate User(s) Detected"
+}
+
+# Check Phone Number is in Valid Format
+Write-Host "Checking Phone Number Formatting"
+$PhoneCheck = CheckPhoneSyntax $inputFile
+if($PhoneCheck -gt 0){
+    Write-Host "Phone Number Validations Failed"
+    exit
+} else {
+    Write-Host "Phone Number Validations Passed"
+}
+
 # If Validation of CSV Passed - Proceed to Load into Target Variable
 $Users = Import-Csv -Path $inputFile
 
@@ -94,9 +245,6 @@ Connect-AzureAD
 # firstname.lastname@student|teacher.ellismarsaliscenter.org
 # firname and lastname will strip all characters that are not A-Z
 # student or teacher will be used based on user role
-
-$Office365StudentTeacherUsers = "C:\Scripts\Office365StudentTeacherUsers.csv"
-$Office365AllUsers = "C:\Scripts\Office365AllUsers.csv"
 
 Write-Host "Processing Target User List"
 $UserNum = 0
@@ -150,8 +298,8 @@ foreach($User in $Users){
 
     #Create User if Needed
     if ($UserExists -eq 0) {
-        if($confirmchanges -eq 1){ Read-Host -Prompt "Press any key to continue or CTRL-C to quit" }
         Write-Host "     Creating New User"
+        if($confirmchanges -eq 1){ Read-Host -Prompt "Press any key to continue or CTRL-C to quit" }
         New-MsolUser -DisplayName $DisplayName -FirstName $FirstName -LastName $LastName -UserPrincipalName $UserPrincipalName -UsageLocation US -LicenseAssignment $TargetLicenseAccountSkuId
     } else {
         Write-Host "     User Already Exists - No Need to Create"
@@ -262,6 +410,15 @@ foreach($User in $Users){
     } else {
         Write-Host "     User Already Administrative Unit Member"
     }
+
+    # Check if User Anchor Exists in Canvas
+    if(ExistsInCanvas $UserPrincipalName){
+        Write-Host "     User Anchor Exists in Canvas"
+    } else {
+        Write-Host "     Creating User Anchor in Canvas"
+        if($confirmchanges -eq 1){ Read-Host -Prompt "Press any key to continue or CTRL-C to quit" }
+        CreateInCanvas $UserPrincipalName
+    }
 }
 
 Write-Host ""
@@ -297,6 +454,7 @@ foreach($aaduser in $aadusers){
                 Write-Host "Processing deletion of $aadupn"
                 if($confirmchanges -eq 1){ Read-Host -Prompt "Press any key to continue or CTRL-C to quit" }
                 Remove-MsolUser -UserPrincipalName $aadupn -Force
+                DeleteInCanvas $aadupn
             } else {
                 Write-Host "   Scheduled for deletion on " $aad_City.City
             }
